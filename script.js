6c4c97e627874bfe1e03433411af5baa6b0b5e90
Mehul <mehulbohra11@gmail.com>
2023-10-20 12:41:47 +0530

    before responsive

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let windowBtn=document.querySelector(".window-button");
+let time=document.querySelector("#time-text")
+let date= document.querySelector("#date")
 windowBtn.addEventListener("click",(e)=>{
     gsap.to(".start-box",{
         top:"50%",
@@ -28,3 +30,10 @@ let clicked=false
         })
     })
 */
+
+const timeDate = ()=>{
+    let nowTimeDate=new Date();
+    time.innerHTML=`${nowTimeDate.getHours()}:${nowTimeDate.getMinutes()}`
+    date.innerHTML=`${nowTimeDate.getDate()}-${nowTimeDate.getMonth()+1}-${nowTimeDate.getFullYear()}`
+}
+setInterval(timeDate,1000)
